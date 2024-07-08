b1d028613ddc6db90c9bc68926ef6a00fc16c247
Asraful Alam Ratul <71044633+ratul701E@users.noreply.github.com>
2024-07-08 13:34:01 +0600

    new controller added

---
+++ job-app-management-system.api/Controllers/FeedbackController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace job_app_management_system.api.Controllers
+{
+    [Route("api/feedback")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        [HttpGet]
+        public bool CheckServerClientConnection()
+        {
+            return true;
+        }
+    }
+}
